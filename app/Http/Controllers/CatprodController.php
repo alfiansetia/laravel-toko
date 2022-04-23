04d4f38ca2784a5fccc25eafbde74687d5f5b092
alfiansetia <alfian.setia100@gmail.com>
2022-04-23 14:11:03 +0700

    first commit

---
+++ app/Http/Controllers/CatprodController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Catprod;
+use Illuminate\Http\Request;
+
+class CatprodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Catprod  $catprod
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Catprod $catprod)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Catprod  $catprod
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Catprod $catprod)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Catprod  $catprod
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Catprod $catprod)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Catprod  $catprod
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Catprod $catprod)
+    {
+        //
+    }
+}
